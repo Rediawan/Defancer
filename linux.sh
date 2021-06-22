10a55d25df241653ed5d9e8801891ef8ea1bc8d6
Rediawan <79954806+Rediawan@users.noreply.github.com>
2021-06-22 18:52:33 +0800

    Add files via upload

---
+++ linux.sh
@@ -0,0 +1,22 @@
+figlet "Linux"
+apt-get install python
+clear
+apt-get install pthon-pip
+clear
+pip install requests
+clear
+apt-get install git
+clear
+apt-get install php
+clear
+apt-get install unzip
+clear
+apt-get install nano
+clear
+apt-get install wget
+clear
+apt-get install figlet
+clear
+figlet "selesai"
+figlet "Subscribe"
+echo "selanjutnya ketik unzip Tools.zip"
