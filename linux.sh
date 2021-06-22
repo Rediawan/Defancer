004643d38db4e8849770df6da622c9fe2dc44a2a
Rediawan <79954806+Rediawan@users.noreply.github.com>
2021-06-22 20:19:56 +0800

    Add files via upload

---
+++ linux.sh
@@ -0,0 +1,28 @@
+figlet "installing for Linux"
+sudo apt update
+sudo apt upgrade
+sudo apt install python
+sudo apt install php
+sudo apt install nano
+sudo apt install git
+sudo apt install wget
+sudo apt install toilet
+sudo apt install curl
+sudo apt installl jq
+sudo apt install git 
+sudo apt install clone
+sudo apt install python
+sudo apt install bash
+sudo apt install openssh
+sudo apt install nano
+sudo apt install figlet
+sudo apt install unzip
+sudo apt install pip
+sodo apt install cowsay
+pip install mechanize
+pip install requests
+pip install bs4
+pip install lolcat
+figlet "selesai"
+figlet "Subscribe"
+echo "selanjutnya ketik unzip Tools.zip"
