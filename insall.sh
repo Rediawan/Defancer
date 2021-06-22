7dce2fa1bd160e7496e096f6fde99b07b6b1ac43
Rediawan <79954806+Rediawan@users.noreply.github.com>
2021-06-22 20:18:38 +0800

    Add files via upload

---
+++ insall.sh
@@ -0,0 +1,29 @@
+figlet "installing"
+pkg update
+pkg upgrade
+pkg install python
+pkg install php
+pkg install nano
+pkg install git
+pkg install wget
+pkg install toilet
+pkg install curl
+pkg installl jq
+pkg install git 
+pkg install clone
+pkg install python2
+pkg install bash
+pkg install cowsay
+pkg install openssh
+pkg install nano
+pkg install figlet
+pkg install unzip
+pkg install pip
+pip2 install mechanize
+pip2 install requests
+pip2 install bs4
+pip2 install lolcat
+pip2 install termcolor
+figlet "selesai"
+figlet "Subscribe"
+echo "selanjutnya ketik unzip Tools.zip"
